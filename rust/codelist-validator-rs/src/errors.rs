cf0cf56c49a35c5898a03f9d49af1306d4f9ed68
Emma <156218556+em-baggie@users.noreply.github.com>
2025-01-03 21:31:47 +0000

    edited functionality of icd10 methods slightly

--- rust/codelist-validator-rs/src/errors.rs
+++ rust/codelist-validator-rs/src/errors.rs
@@ -2,20 +2,22 @@ use core::num;
 
 /// Enum to represent the different types of errors that can occur in the codelist-validator library
 ///
+/// * `InvalidCodeLength` - An error that occurs when the code is not the correct length
+/// * `ParseIntError` - An error that occurs when the code is not composed of numerical characters
+/// * `InvalidCodeContents` - An error that occurs when the code does not match the expected format
 
 #[derive(Debug, thiserror::Error, thiserror_ext::Construct, Clone)]
 pub enum CodeListValidatorError {
     #[error("Code {code} is an invalid length. Reason: {reason}")]
     InvalidCodeLength { code: String, reason: String },
 
-    #[error("Code is not numerical: {0}")]
+    #[error("Code is not composed of numerical characters: {0}")]
     #[construct(skip)]
     ParseIntError(#[from] num::ParseIntError),
 
-    #[error("Error initialising REGEX: {0}")]
-    #[construct(skip)]
-    RegexInitError(#[from] regex::Error),
-
     #[error("Code {code} contents is invalid. Reason: {reason}")]
     InvalidCodeContents { code: String, reason: String },
+
+    #[error("Some codes in the list are invalid. Details: {reasons:?}")]
+    InvalidCodelist { reasons: Vec<(String, String)> },
 }
--- rust/codelist-validator-rs/src/icd10_validator.rs
+++ rust/codelist-validator-rs/src/icd10_validator.rs
@@ -3,23 +3,38 @@ use regex::Regex;
 use std::sync::LazyLock;
 use crate::errors::CodeListValidatorError;
 
+/// ICD10 code regex pattern
+
 static REGEX: LazyLock<Regex> = LazyLock::new(|| {
     Regex::new(r"^[A-Z]\d{2}(X|(\.\d{1,3})?|\d{1,4})?$").expect("Unable to create regex")
 });
 
+/// ICD10 validator trait
+/// 
+/// `validate_code`: validates a single ICD10 code
+/// `validate_all_code`: validates all ICD10 codes in the codelist
 pub trait ICD10Validator {
     fn validate_code(&self, code: &str) -> Result<(), CodeListValidatorError>; // for 1 code
     fn validate_all_code(&self) -> Result<(), CodeListValidatorError>;
 }
 
-// Implement the `OPCSValidator` trait for `CodeList`
+/// Implementation of ICD10Validator trait for the CodeList struct
 impl ICD10Validator for CodeList {
+    /// Validate a single ICD10 code
+    /// 
+    /// # Arguments
+    /// 
+    /// * `code`: the code to validate
+    /// 
+    /// # Returns
+    /// 
+    /// * `Result<(), String>`: unit type if the code is valid, otherwise an error containing the code and the reason the code is invalid
     fn validate_code(&self, code: &str) -> Result<(), CodeListValidatorError> {
         if code.len() > 7 {
-            return Err(CodeListValidatorError::invalid_code_length(code, "ICD10 code is not greater than 7 in length"))
+            return Err(CodeListValidatorError::invalid_code_length(code, "ICD10 code is not greater than 7 characters in length"));
         }
 
-        let re = &REGEX;
+        let re = &*REGEX;
 
         if !re.is_match(code) {
             return Err(CodeListValidatorError::invalid_code_contents(
@@ -27,45 +42,76 @@ impl ICD10Validator for CodeList {
                 format!("ICD10 code {} does not match the expected format", code), // Corrected string interpolation
             ));
         }
+
         Ok(())
     }
 
+    /// Validate all ICD10 codes in the codelist
+    /// 
+    /// # Returns
+    /// 
+    /// * `Result<(), CodeListValidatorError>`: unit type if all codes are valid in the codelist, otherwise an error containing all invalid codes and the reason for being invalid
     fn validate_all_code(&self) -> Result<(), CodeListValidatorError> {
+        let mut invalid_codes = Vec::new();
+
         for code_entry in self.entries.iter() {
             let code = &code_entry.code;
-            self.validate_code(code)?;
+            if let Err(err) = self.validate_code(code) {
+                let error_reason = format!("{}", err);
+                invalid_codes.push((code.clone(), error_reason));
+            }
         }
-        Ok(())
+
+        if invalid_codes.is_empty() {
+            Ok(())
+        } else {
+            Err(CodeListValidatorError::invalid_codelist(invalid_codes))
+        }
     }
 }
 
-// def validate_icd10_code(code: str) -> bool:
-// """
-//         Validate the form of an ICD-10 code.
-//
-//         The rules are:
-//         - The code must be 7 characters or less
-//         - The first character must be a letter
-//         - The second and third characters must be numbers
-//         - The fourth character must be a dot, or a number or X
-//         - If the fourth character is a dot, there must be at least 1 number after the dot
-//         - If the fourth character is a X, there are no further characters
-//         - The fifth to seventh characters must be numbers if present
-//
-//         Args:
-//             code (str): The code to validate.
-//
-//         Returns:
-//             bool: True if the code is valid, False otherwise.
-//         """
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use codelist_rs::metadata::{ Metadata, MetadataSource };
+    use codelist_rs::codelist::{ CodeList, CodeListType };
+    use codelist_rs::errors::CodeListError;
+
+    // Helper function to create test metadata
+    fn create_test_metadata() -> Metadata {
+        Metadata {
+            source: MetadataSource::ManuallyCreated,
+            authors: Some(vec!["Caroline Morton".to_string()]),
+            version: Some("2024-12-19".to_string()),
+            description: Some("A test codelist".to_string()),
+        }
+    }
+
+    // Helper function to create a test codelist with two entries, default options and test metadata
+    fn create_test_codelist() -> Result<CodeList, CodeListError> {
+        let mut codelist = CodeList::new(CodeListType::ICD10, create_test_metadata(), None);
+        codelist.add_entry("R65.2".to_string(), "Severe sepsis".to_string())?;
+        codelist.add_entry("A48.51".to_string(), "Infant botulism".to_string())?;
+        
+        Ok(codelist)
+    }
 
 
-//TODO: tests
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//
-// }
-//
+    #[test]
+    fn test_validate_code_with_valid_code() {;
+
+    }
+
+    #[test]
+    fn test_validate_code_with_invalid_code_length() {;
+
+    }
+
+    #[test]
+    fn test_validate_code_with_invalid_code_content() {;
+
+    }
+
+    #[test]
+    fn test_validate_all_code_valid_codelist() {}
+}
