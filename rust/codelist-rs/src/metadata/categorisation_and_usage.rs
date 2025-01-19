68c7e3386b5009cd7eb2ba20d3c7e25a79c96c26
Emma <156218556+em-baggie@users.noreply.github.com>
2025-01-19 22:33:41 +0000

    added new fn to categorisation and usage

--- rust/codelist-rs/src/metadata/categorisation_and_usage.rs
+++ rust/codelist-rs/src/metadata/categorisation_and_usage.rs
@@ -13,6 +13,15 @@ pub struct CategorisationAndUsage {
 }
 
 impl CategorisationAndUsage {
+    /// Create new CategorisationAndUsage
+    ///
+    /// # Arguments
+    /// * `tags` - The tags of the codelist
+    /// * `usage` - The usage of the codelist
+    /// * `license` - The license of the codelist
+    ///
+    /// # Returns
+    /// * `CategorisationAndUsage` - The new CategorisationAndUsage
     pub fn new(tags: Option<HashSet<String>>, usage: Option<HashSet<String>>, license: Option<String>) -> Self {
         Self {
             tags: tags.unwrap_or_default(),
@@ -26,4 +35,18 @@ impl CategorisationAndUsage {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // Helper function to create a test categorisation and usage
+    fn test_categorisation_and_usage() -> CategorisationAndUsage {
+        CategorisationAndUsage::new(None, Some(HashSet::from(["usage1".to_string(), "usage2".to_string()])), Some("license1".to_string()))
+    }
+
+    #[test] 
+    fn test_new() -> Result<(), CodeListError> {
+        let categorisation_and_usage = test_categorisation_and_usage();
+        assert_eq!(categorisation_and_usage.tags, HashSet::new());
+        assert_eq!(categorisation_and_usage.usage, HashSet::from(["usage1".to_string(), "usage2".to_string()]));
+        assert_eq!(categorisation_and_usage.license, Some("license1".to_string()));
+        Ok(())
+    }
 }
