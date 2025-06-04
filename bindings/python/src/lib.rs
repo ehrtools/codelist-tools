8ae2271cdefe52ebdceb55a567ec9bee1f074f34
Tim Heaney <oylenshpeegul@proton.me>
2025-06-04 15:28:20 -0400

    Hm. is rustfmt from my editor not the same as cargo fmt?

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -18,9 +18,7 @@ fn codelists_rs(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_submodule(&codelist_module)?;
 
     // Register it globally under the full dotted path
-    py.import("sys")?
-        .getattr("modules")?
-        .set_item("codelists_rs.codelist", codelist_module)?;
+    py.import("sys")?.getattr("modules")?.set_item("codelists_rs.codelist", codelist_module)?;
 
     // Add factory submodule
     let factory_module = PyModule::new(py, "factory")?;
