4df5db7c3a08824aa4a1fdc96b39467d95294df5
Tim Heaney <oylenshpeegul@proton.me>
2025-06-04 14:41:29 -0400

    migrate pyo3 from 0.20 to 0.21
    Followed steps listed here:
    https://pyo3.rs/v0.25.0/migration.html#from-020-to-021

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -11,22 +11,22 @@ use factory::PyCodeListFactory;
 
 /// Top-level Python module `codelists_rs`
 #[pymodule]
-fn codelists_rs(py: Python, m: &PyModule) -> PyResult<()> {
+fn codelists_rs(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     // Add codelist submodule
-    let codelist_module = PyModule::new(py, "codelist")?;
+    let codelist_module = PyModule::new_bound(py, "codelist")?;
     codelist_module.add_class::<PyCodeList>()?;
-    m.add_submodule(codelist_module)?;
+    m.add_submodule(&codelist_module)?;
 
     // Register it globally under the full dotted path
-    py.import("sys")?.getattr("modules")?.set_item("codelists_rs.codelist", codelist_module)?;
+    py.import_bound("sys")?.getattr("modules")?.set_item("codelists_rs.codelist", codelist_module)?;
 
     // Add factory submodule
-    let factory_module = PyModule::new(py, "factory")?;
+    let factory_module = PyModule::new_bound(py, "factory")?;
     factory_module.add_class::<PyCodeListFactory>()?;
-    m.add_submodule(factory_module)?;
+    m.add_submodule(&factory_module)?;
 
     // Register it globally under the full dotted path
-    py.import("sys")?.getattr("modules")?.set_item("codelists_rs.factory", factory_module)?;
+    py.import_bound("sys")?.getattr("modules")?.set_item("codelists_rs.factory", factory_module)?;
 
     Ok(())
 }
