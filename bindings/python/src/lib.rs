7371dfab52546bd79a305460cad98adc4501a76e
Tim Heaney <oylenshpeegul@gmail.com>
2025-06-05 05:11:03 -0400

    Update PyO3 to 0.25 (#82)
    * migrate pyo3 from 0.20 to 0.21
    
    Followed steps listed here:
    https://pyo3.rs/v0.25.0/migration.html#from-020-to-021
    
    * migrate pyo3 from 0.21 to 0.22
    
    Followed the guide here:
    https://pyo3.rs/v0.25.0/migration.html#from-021-to-022
    
    This allowed upgrading Python from 3.12 to 3.13
    
    * migrate from pyo3 0.22 to 0.23
    
    * migrate pyo3 from 0.23 to 0.25
    
    changed 0.23 to 0.24 with no issues
    changed 0.24 to 0.25 with no issues
    
    * Hm. is rustfmt from my editor not the same as cargo fmt?
    
    * test Python 3.13 too!
    
    * whitepace, really?
    
    * no, comma...grr
    
    * wot?
    
    * testing if this works!
    
    ---------
    
    Co-authored-by: Tim Heaney <oylenshpeegul@proton.me>
    Co-authored-by: CarolineMorton <carolinemorton@gmx.com>

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -11,11 +11,11 @@ use factory::PyCodeListFactory;
 
 /// Top-level Python module `codelists_rs`
 #[pymodule]
-fn codelists_rs(py: Python, m: &PyModule) -> PyResult<()> {
+fn codelists_rs(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     // Add codelist submodule
     let codelist_module = PyModule::new(py, "codelist")?;
     codelist_module.add_class::<PyCodeList>()?;
-    m.add_submodule(codelist_module)?;
+    m.add_submodule(&codelist_module)?;
 
     // Register it globally under the full dotted path
     py.import("sys")?.getattr("modules")?.set_item("codelists_rs.codelist", codelist_module)?;
@@ -23,7 +23,7 @@ fn codelists_rs(py: Python, m: &PyModule) -> PyResult<()> {
     // Add factory submodule
     let factory_module = PyModule::new(py, "factory")?;
     factory_module.add_class::<PyCodeListFactory>()?;
-    m.add_submodule(factory_module)?;
+    m.add_submodule(&factory_module)?;
 
     // Register it globally under the full dotted path
     py.import("sys")?.getattr("modules")?.set_item("codelists_rs.factory", factory_module)?;
