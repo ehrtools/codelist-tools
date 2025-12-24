5344a3725645f94a841108f573b610758b950509
Emma Baghurst <156218556+em-baggie@users.noreply.github.com>
2025-12-24 17:35:51 +0000

    Add json config

--- rust/codelist-builder-rs/src/errors.rs
+++ rust/codelist-builder-rs/src/errors.rs
@@ -2,6 +2,7 @@
 
 /// Enum to represent the different types of errors that can occur in the
 /// codelist-builder library
+use std::io;
 
 #[derive(Debug, thiserror::Error, thiserror_ext::Construct)]
 pub enum CodeListBuilderError {
@@ -14,9 +15,20 @@ pub enum CodeListBuilderError {
     #[error("HTTP error code: {code}: {body}")]
     HttpErrorCode { code: String, body: String },
 
+    #[error("URL not found in config for year {year}")]
+    UrlNotFound { year: String },
+
     #[error("HTTP request error: {0}")]
     #[construct(skip)]
     ReqwestError(#[from] reqwest::Error),
+
+    #[error("IO error: {0}")]
+    #[construct(skip)]
+    IOError(#[from] io::Error),
+
+    #[error("JSON error: {0}")]
+    #[construct(skip)]
+    JSONError(#[from] serde_json::Error),
 
     #[error("CSV error: {0}")]
     #[construct(skip)]
--- rust/codelist-builder-rs/tests/download_usage.rs
+++ rust/codelist-builder-rs/tests/download_usage.rs
@@ -1,24 +1,21 @@
 use codelist_builder_rs::errors::CodeListBuilderError;
 use codelist_builder_rs::snomed_usage_data::SnomedUsageData;
-use codelist_builder_rs::usage_year::UsageYear;
 
 #[tokio::test]
 async fn test_download_usage() -> Result<(), CodeListBuilderError> {
-    let base_url = "https://files.digital.nhs.uk";
-
-    let result_2011_12 = SnomedUsageData::download_usage(base_url, UsageYear::Y2011_12).await?;
-    let result_2012_13 = SnomedUsageData::download_usage(base_url, UsageYear::Y2012_13).await?;
-    let result_2013_14 = SnomedUsageData::download_usage(base_url, UsageYear::Y2013_14).await?;
-    let result_2014_15 = SnomedUsageData::download_usage(base_url, UsageYear::Y2014_15).await?;
-    let result_2015_16 = SnomedUsageData::download_usage(base_url, UsageYear::Y2015_16).await?;
-    let result_2016_17 = SnomedUsageData::download_usage(base_url, UsageYear::Y2016_17).await?;
-    let result_2017_18 = SnomedUsageData::download_usage(base_url, UsageYear::Y2017_18).await?;
-    let result_2018_19 = SnomedUsageData::download_usage(base_url, UsageYear::Y2018_19).await?;
-    let result_2019_20 = SnomedUsageData::download_usage(base_url, UsageYear::Y2019_20).await?;
-    let result_2020_21 = SnomedUsageData::download_usage(base_url, UsageYear::Y2020_21).await?;
-    let result_2021_22 = SnomedUsageData::download_usage(base_url, UsageYear::Y2021_22).await?;
-    let result_2022_23 = SnomedUsageData::download_usage(base_url, UsageYear::Y2022_23).await?;
-    let result_2023_24 = SnomedUsageData::download_usage(base_url, UsageYear::Y2023_24).await?;
+    let result_2011_12 = SnomedUsageData::download_usage("2011-12", None).await?;
+    let result_2012_13 = SnomedUsageData::download_usage("2012-13", None).await?;
+    let result_2013_14 = SnomedUsageData::download_usage("2013-14", None).await?;
+    let result_2014_15 = SnomedUsageData::download_usage("2014-15", None).await?;
+    let result_2015_16 = SnomedUsageData::download_usage("2015-16", None).await?;
+    let result_2016_17 = SnomedUsageData::download_usage("2016-17", None).await?;
+    let result_2017_18 = SnomedUsageData::download_usage("2017-18", None).await?;
+    let result_2018_19 = SnomedUsageData::download_usage("2018-19", None).await?;
+    let result_2019_20 = SnomedUsageData::download_usage("2019-20", None).await?;
+    let result_2020_21 = SnomedUsageData::download_usage("2020-21", None).await?;
+    let result_2021_22 = SnomedUsageData::download_usage("2021-22", None).await?;
+    let result_2022_23 = SnomedUsageData::download_usage("2022-23", None).await?;
+    let result_2023_24 = SnomedUsageData::download_usage("2023-24", None).await?;
 
     assert!(!result_2011_12.usage_data.is_empty());
     assert!(!result_2012_13.usage_data.is_empty());
